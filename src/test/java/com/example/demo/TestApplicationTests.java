8a9a4de1912e305453a667d52cf46bb9009946e1
sowon <js4848cc@gmail.com>
2025-12-05 02:05:14 +0900

    fix: ?뚯뒪???섍꼍 ?ㅼ젙 異붽? (JWT, Google OAuth ?붾? 媛?諛?Mock 鍮??ㅼ젙)

--- src/test/java/com/example/demo/TestApplicationTests.java
+++ src/test/java/com/example/demo/TestApplicationTests.java
@@ -1,10 +1,39 @@
 package com.example.demo;
 
+import com.example.demo.auth.google.GoogleIdTokenVerifierService;
+import com.example.demo.auth.service.SocialAuthService;
+import com.example.demo.domain.repository.RefreshTokenRepository;
+import com.example.demo.domain.repository.UserRepository;
+import com.example.demo.security.jwt.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "jwt.secret=dGVzdC1zZWNyZXQta2V5LWZvci10ZXN0aW5nLXB1cnBvc2VzLW9ubHk=",
+    "jwt.access-token-validity-ms=3600000",
+    "jwt.refresh-token-validity-ms=2592000000",
+    "auth.google.client-id.ios=test-ios-client-id.apps.googleusercontent.com",
+    "auth.google.client-id.android=test-android-client-id.apps.googleusercontent.com"
+})
 class TestApplicationTests {
+
+    @MockBean
+    private GoogleIdTokenVerifierService googleIdTokenVerifierService;
+    
+    @MockBean
+    private UserRepository userRepository;
+    
+    @MockBean
+    private RefreshTokenRepository refreshTokenRepository;
+    
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+    
+    @MockBean
+    private SocialAuthService socialAuthService;
 
 	@Test
 	void contextLoads() {
